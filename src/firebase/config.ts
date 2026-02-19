e0d14f18283b576f2f1c8833414b8922975f6b23
Jan Gerek <gerektipcars@gmail.com>
2026-02-19 10:22:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8802850490-fd2b1",
+  "appId": "1:1075240292352:web:cd222b4a07969964d948e5",
+  "apiKey": "AIzaSyBMYydA_0_THuMhnvftQlhQClHpntd3I5M",
+  "authDomain": "studio-8802850490-fd2b1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1075240292352"
+};
